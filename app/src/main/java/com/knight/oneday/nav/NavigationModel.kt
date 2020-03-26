6022a5ce89e098f925e0a927a44a15756e679ca1
fenglj <Lee_Mofeel@163.com>
2020-03-26 16:23:46 +0800

    -- 60% 首页

--- app/src/main/java/com/knight/oneday/nav/NavigationModel.kt
+++ app/src/main/java/com/knight/oneday/nav/NavigationModel.kt
@@ -3,28 +3,43 @@ package com.knight.oneday.nav
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.knight.oneday.R
+import com.knight.oneday.data.EventTag
+import com.knight.oneday.utilities.getString
 
 object NavigationModel {
 
     private var navigationMenuItems = mutableListOf(
         NavigationModelItem.NavMenuItem(
             id = 0,
+            icon = R.drawable.ic_one_day_home,
+            titleRes = R.string.nav_menu_home,
+            checked = false
+        ),
+        NavigationModelItem.NavMenuItem(
+            id = 1,
             icon = R.drawable.ic_one_day_about,
             titleRes = R.string.nav_menu_about,
             checked = false
         ),
         NavigationModelItem.NavMenuItem(
-            id = 1,
+            id = 2,
             icon = R.drawable.ic_one_day_chart,
             titleRes = R.string.nav_menu_chart,
             checked = false
         ),
         NavigationModelItem.NavMenuItem(
-            id = 2,
+            id = 3,
             icon = R.drawable.ic_one_day_settings,
             titleRes = R.string.nav_menu_settings,
             checked = false
         )
+    )
+
+    private val navigationTags = mutableListOf(
+        NavigationModelItem.NavEventTag(getString(R.string.tag_life)),
+        NavigationModelItem.NavEventTag(getString(R.string.tag_work)),
+        NavigationModelItem.NavEventTag(getString(R.string.tag_entertainment)),
+        NavigationModelItem.NavEventTag(getString(R.string.tag_health))
     )
 
     private val _navigationList: MutableLiveData<List<NavigationModelItem>> = MutableLiveData()
@@ -50,8 +65,8 @@ object NavigationModel {
     }
 
     private fun postListUpdate() {
-        val newList = navigationMenuItems
-
+        val newList =
+            navigationMenuItems + (NavigationModelItem.NavDivider(getString(R.string.divider_tag))) + navigationTags
         _navigationList.value = newList
     }
 
