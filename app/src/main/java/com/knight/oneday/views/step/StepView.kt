0b5dcb63b4dfae2a759e10250c17a9a6560e59d5
fenglj <Lee_Mofeel@163.com>
2020-04-22 09:44:01 +0800

    -- 定义StepView以及其属性

---
+++ app/src/main/java/com/knight/oneday/views/step/StepView.kt
@@ -0,0 +1,17 @@
+package com.knight.oneday.views.step
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Create by FLJ in 2020/4/22 9:22
+ * 步骤View
+ */
+class StepView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    var status: Int = 0
+
+}
