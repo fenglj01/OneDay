d1775fd6a0578b85b0e37577d43693c2a07f96b5
fenglj <Lee_Mofeel@163.com>
2020-05-14 09:09:35 +0800

    -- ap pm 选择器UI

--- app/src/main/java/com/knight/oneday/views/choice/AmPmChoiceView.kt
+++ app/src/main/java/com/knight/oneday/views/choice/AmPmChoiceView.kt
@@ -2,8 +2,10 @@ package com.knight.oneday.views.choice
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.knight.oneday.R
 
 /**
  * Create by FLJ in 2020/5/13 16:02
@@ -18,7 +20,7 @@ class AmPmChoiceView @JvmOverloads constructor(
     }
 
     private fun initAttr() {
-
+        LayoutInflater.from(context).inflate(R.layout.am_pm_choice_view, this, true)
     }
 
     interface OnChoiceFinish {
