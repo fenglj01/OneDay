1ab42e9c8e0d13903b5f1a155dedb52528e7ae0a
fenglj <Lee_Mofeel@163.com>
2020-05-19 13:35:49 +0800

    -- BaseSettingFragment 封装 以及通用样式的定版

---
+++ app/src/main/java/com/knight/oneday/BaseSettingFragment.kt
@@ -0,0 +1,44 @@
+package com.knight.oneday
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.preference.PreferenceFragmentCompat
+import kotlinx.android.synthetic.main.fragment_preferences.*
+
+/**
+ * Create by FLJ in 2020/5/19 13:22
+ * 设置界面的提取
+ */
+abstract class BaseSettingFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(preferenceXml(), rootKey)
+        afterPreferenceXml()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setting_toolbar.title = titleString()
+        setting_toolbar.setNavigationOnClickListener {
+            beforeNavigateUp()
+            findNavController().navigateUp()
+        }
+    }
+
+    abstract fun preferenceXml(): Int
+
+    protected open fun titleString(): String = getString(R.string.menu_setting)
+
+    protected open fun beforeNavigateUp() {
+
+    }
+
+    protected open fun afterPreferenceXml() {
+
+    }
+}
--- app/src/main/java/com/knight/oneday/SettingFragment.kt
+++ app/src/main/java/com/knight/oneday/SettingFragment.kt
@@ -29,38 +29,37 @@ private const val ARG_PARAM2 = "param2"
 /**
  * 设置界面
  */
-class SettingFragment : PreferenceFragmentCompat() {
+class SettingFragment : BaseSettingFragment() {
 
-    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
-        val themePreference: ListPreference? = findPreference(THEME_KEY)
-        val themeArrayOptionsAboveQ = resources.getStringArray(R.array.theme_options_above_q)
-        val themeArrayOptionsBelowQ = resources.getStringArray(R.array.theme_options_below_q)
-        themePreference?.entries =
-            if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ else themeArrayOptionsBelowQ
-        val themeArrayValues = resources.getStringArray(R.array.theme_values)
-        themePreference?.summaryProvider =
-            Preference.SummaryProvider<ListPreference> { preference ->
-                when (preference.value) {
-//                 Light Theme
-                    themeArrayValues[0] -> themeArrayOptionsAboveQ[0]
-//                 Dark Theme
-                    themeArrayValues[1] -> themeArrayOptionsAboveQ[1]
-//                 System Defined Theme/Auto Battery
-                    themeArrayValues[2] -> if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ[2] else themeArrayOptionsBelowQ[2]
-                    else -> getString(R.string.def)
-                }
-            }
+    /* override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+         setPreferencesFromResource(R.xml.preferences, rootKey)
+         val themePreference: ListPreference? = findPreference(THEME_KEY)
+         val themeArrayOptionsAboveQ = resources.getStringArray(R.array.theme_options_above_q)
+         val themeArrayOptionsBelowQ = resources.getStringArray(R.array.theme_options_below_q)
+         themePreference?.entries =
+             if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ else themeArrayOptionsBelowQ
+         val themeArrayValues = resources.getStringArray(R.array.theme_values)
+         themePreference?.summaryProvider =
+             Preference.SummaryProvider<ListPreference> { preference ->
+                 when (preference.value) {
+ //                 Light Theme
+                     themeArrayValues[0] -> themeArrayOptionsAboveQ[0]
+ //                 Dark Theme
+                     themeArrayValues[1] -> themeArrayOptionsAboveQ[1]
+ //                 System Defined Theme/Auto Battery
+                     themeArrayValues[2] -> if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ[2] else themeArrayOptionsBelowQ[2]
+                     else -> getString(R.string.def)
+                 }
+             }
+     }*/
+
+    override fun preferenceXml(): Int = R.xml.preferences
+
+    override fun afterPreferenceXml() {
+
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setting_toolbar.title = getString(R.string.menu_setting)
-        setting_toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
-    }
-
-
-    companion object {
+    /*companion object {
         const val THEME_KEY = "theme"
-    }
+    }*/
 }
