f59a13987d91c74135c613f3fd7716f2c74fded0
fenglj <Lee_Mofeel@163.com>
2020-05-19 14:35:40 +0800

    -- code review

--- app/src/main/java/com/knight/oneday/SettingFragment.kt
+++ app/src/main/java/com/knight/oneday/SettingFragment.kt
@@ -31,35 +31,10 @@ private const val ARG_PARAM2 = "param2"
  */
 class SettingFragment : BaseSettingFragment() {
 
-    /* override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-         setPreferencesFromResource(R.xml.preferences, rootKey)
-         val themePreference: ListPreference? = findPreference(THEME_KEY)
-         val themeArrayOptionsAboveQ = resources.getStringArray(R.array.theme_options_above_q)
-         val themeArrayOptionsBelowQ = resources.getStringArray(R.array.theme_options_below_q)
-         themePreference?.entries =
-             if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ else themeArrayOptionsBelowQ
-         val themeArrayValues = resources.getStringArray(R.array.theme_values)
-         themePreference?.summaryProvider =
-             Preference.SummaryProvider<ListPreference> { preference ->
-                 when (preference.value) {
- //                 Light Theme
-                     themeArrayValues[0] -> themeArrayOptionsAboveQ[0]
- //                 Dark Theme
-                     themeArrayValues[1] -> themeArrayOptionsAboveQ[1]
- //                 System Defined Theme/Auto Battery
-                     themeArrayValues[2] -> if (Build.VERSION.SDK_INT >= 29) themeArrayOptionsAboveQ[2] else themeArrayOptionsBelowQ[2]
-                     else -> getString(R.string.def)
-                 }
-             }
-     }*/
-
     override fun preferenceXml(): Int = R.xml.preferences
 
     override fun afterPreferenceXml() {
 
     }
 
-    /*companion object {
-        const val THEME_KEY = "theme"
-    }*/
 }
