f9356d26062e7a5abb0f873464807d9e9f1d1df5
fenglj <Lee_Mofeel@163.com>
2020-05-19 10:14:31 +0800

    -- 设置界面toolbar相关 取消了mainAct得fullScreen 暂时没有发现影响到了其他得界面 如果影响再做其他处理

--- app/src/main/java/com/knight/oneday/SettingFragment.kt
+++ app/src/main/java/com/knight/oneday/SettingFragment.kt
@@ -2,14 +2,23 @@ package com.knight.oneday
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginTop
+import androidx.navigation.fragment.findNavController
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.knight.oneday.generated.callback.OnClickListener
+import com.knight.oneday.views.showSnackBar
 import kotlinx.android.synthetic.main.fragment_preferences.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -47,8 +56,9 @@ class SettingFragment : PreferenceFragmentCompat() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setting_toolbar.title = getString(R.string.menu_setting)
-
+        setting_toolbar.setNavigationOnClickListener { findNavController().navigateUp() }
     }
+
 
     companion object {
         const val THEME_KEY = "theme"
