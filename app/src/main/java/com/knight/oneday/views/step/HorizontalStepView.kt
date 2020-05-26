7c1ba5fddce535baece6549d40956aa1bacaf467
fenglj <Lee_Mofeel@163.com>
2020-05-26 16:08:34 +0800

    -- HorizontalStepView attrs 的封装

---
+++ app/src/main/java/com/knight/oneday/views/step/HorizontalStepView.kt
@@ -0,0 +1,15 @@
+package com.knight.oneday.views.step
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.HorizontalScrollView
+
+/**
+ * Create by FLJ in 2020/5/26 16:07
+ * 水平方向可滑动的步骤View
+ */
+class HorizontalStepView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : HorizontalScrollView(context, attrs, defStyleAttr) {
+
+}
