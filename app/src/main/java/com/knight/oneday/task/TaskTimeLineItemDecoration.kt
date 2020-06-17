91c0fde26eec960ddb264ff0ad36067fd1363ad6
fenglj <Lee_Mofeel@163.com>
2020-06-17 14:30:36 +0800

    -- TaskFragment TaskTimeLineRecyclerView attrs

--- app/src/main/java/com/knight/oneday/task/TaskTimeLineItemDecoration.kt
+++ app/src/main/java/com/knight/oneday/task/TaskTimeLineItemDecoration.kt
@@ -2,12 +2,17 @@ package com.knight.oneday.task
 
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.Paint
 import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
 import android.view.View
 import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.annotation.Dimension
 import androidx.recyclerview.widget.RecyclerView
 import com.knight.oneday.utilities.TaskType
+import com.knight.oneday.utilities.dp
 import com.knight.oneday.utilities.sp
 
 /**
@@ -17,6 +22,19 @@ import com.knight.oneday.utilities.sp
 class TaskTimeLineItemDecoration private constructor(private val params: TaskTimeLineParams) :
     RecyclerView.ItemDecoration() {
 
+    private val textPaint: TextPaint = TextPaint(Paint.ANTI_ALIAS_FLAG)
+    private val timeLinePaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val timeLineIconPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    init {
+        textPaint.apply {
+            textSize = params.timeLineTextSize
+            color = params.timeLineUnFinishedTextColor
+        }
+        timeLinePaint.apply {
+            color = params.timeLineUnFinishedFillColor
+        }
+    }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
         super.onDraw(c, parent, state)
@@ -34,15 +52,25 @@ class TaskTimeLineItemDecoration private constructor(private val params: TaskTim
     interface TimeLineContentCallback {
         fun getTime(position: Int): String
         fun getTaskType(position: Int): TaskType
+        fun getTaskStatus(position: Int): Int
     }
 
     class TaskTimeLineParams {
+
         @ColorInt
-        var timeLineUnFinishedColor: Int = Color.BLACK
+        var timeLineColor: Int = Color.BLACK
         @ColorInt
-        var timeLineFinishedColor: Int = Color.GRAY
+        var timeLineUnFinishedFillColor: Int = Color.BLACK
         @ColorInt
-        var timeLineExpiredColor: Int = Color.RED
+        var timeLineUnFinishedStrokeColor: Int = Color.GRAY
+        @ColorInt
+        var timeLineFinishedFillColor: Int = Color.GRAY
+        @ColorInt
+        var timeLineFinishedStrokeColor: Int = Color.GRAY
+        @ColorInt
+        var timeLineExpiredFillColor: Int = Color.RED
+        @ColorInt
+        var timeLineExpiredStrokeColor: Int = Color.RED
 
         var timeLineTextSize: Float = 12F.sp
         @ColorInt
@@ -52,26 +80,60 @@ class TaskTimeLineItemDecoration private constructor(private val params: TaskTim
         @ColorInt
         var timeLineExpiredTextColor = Color.RED
 
+        @Dimension
+        var timeLineCircleRadius = 24f.dp
+        @Dimension
+        var timeLineWidth = 2f.dp
+
+        var timeLineStyle = 0
+
+        var timeLineUnfinishedIcon: Drawable? = null
+        var timeLineFinishedIcon: Drawable? = null
+        var timeLineExpiredIcon: Drawable? = null
+
         var timeLineCallback: TimeLineContentCallback? = null
 
     }
 
     companion object BUILD {
 
+        const val STYLE_FILL = 0
+        const val STYLE_FILL_STROKE = 1
+
         private val timeLineParams = TaskTimeLineParams()
 
-        fun setTimeLineUnFinishedColor(@ColorInt color: Int): BUILD {
-            timeLineParams.timeLineUnFinishedColor = color
+        fun setTimeLineColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineColor = color
             return this
         }
 
-        fun setTimeLineFinishedColor(@ColorInt color: Int): BUILD {
-            timeLineParams.timeLineFinishedColor = color
+        fun setTimeLineUnFinishedFillColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineUnFinishedFillColor = color
             return this
         }
 
-        fun setTimeLineExpiredColor(@ColorInt color: Int): BUILD {
-            timeLineParams.timeLineExpiredColor = color
+        fun setTimeLineUnFinishedStrokeColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineUnFinishedStrokeColor = color
+            return this
+        }
+
+        fun setTimeLineFinishedFillColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineFinishedFillColor = color
+            return this
+        }
+
+        fun setTimeLineFinishedStrokeColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineFinishedStrokeColor = color
+            return this
+        }
+
+        fun setTimeLineExpiredFillColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineExpiredFillColor = color
+            return this
+        }
+
+        fun setTimeLineExpiredStrokeColor(@ColorInt color: Int): BUILD {
+            timeLineParams.timeLineExpiredStrokeColor = color
             return this
         }
 
@@ -97,6 +159,36 @@ class TaskTimeLineItemDecoration private constructor(private val params: TaskTim
 
         fun setTimeLineContentCallback(callback: TimeLineContentCallback): BUILD {
             timeLineParams.timeLineCallback = callback
+            return this
+        }
+
+        fun setTimeLineWidth(@Dimension timeLineWidth: Float): BUILD {
+            timeLineParams.timeLineWidth = timeLineWidth
+            return this
+        }
+
+        fun setTimeLineCircleRadius(@Dimension timeLineCircleRadius: Float): BUILD {
+            timeLineParams.timeLineCircleRadius = timeLineCircleRadius
+            return this
+        }
+
+        fun setTimeLineFinishedIcon(timeLineFinishedIcon: Drawable?): BUILD {
+            timeLineParams.timeLineFinishedIcon = timeLineFinishedIcon
+            return this
+        }
+
+        fun setTimeLineUnfinishedIcon(timeLineUnfinishedIcon: Drawable?): BUILD {
+            timeLineParams.timeLineUnfinishedIcon = timeLineUnfinishedIcon
+            return this
+        }
+
+        fun setTimeLineExpiredIcon(timeLineExpiredIcon: Drawable?): BUILD {
+            timeLineParams.timeLineExpiredIcon = timeLineExpiredIcon
+            return this
+        }
+
+        fun setTimeLineStyle(timeLineStyle: Int): BUILD {
+            timeLineParams.timeLineStyle = timeLineStyle
             return this
         }
 
---
+++ app/src/main/java/com/knight/oneday/task/TaskTimeLineRecyclerView.kt
@@ -0,0 +1,18 @@
+package com.knight.oneday.task
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Create by FLJ in 2020/6/17 13:30
+ */
+class TaskTimeLineRecyclerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    init {
+        
+    }
+
+}
