8cc35f39ee16cf2296559aee8d80fb8adf359ba5
fenglj <Lee_Mofeel@163.com>
2020-06-22 10:42:35 +0800

    -- TaskFragment timeLine style and item task type icon

--- app/src/main/java/com/knight/oneday/task/TaskBindingAdapter.kt
+++ app/src/main/java/com/knight/oneday/task/TaskBindingAdapter.kt
@@ -1,10 +1,15 @@
 package com.knight.oneday.task
 
+import android.annotation.SuppressLint
 import android.view.View
+import android.widget.ImageView
+import androidx.annotation.IntegerRes
 import androidx.databinding.BindingAdapter
 import com.haibin.calendarview.CalendarView
+import com.knight.oneday.R
 import com.knight.oneday.calendar.CalendarToolView
 import com.knight.oneday.data.Task
+import com.knight.oneday.utilities.TaskType
 
 @BindingAdapter("bindSelectedDay")
 fun CalendarToolView.bindSelectedDay(day: String) {
@@ -14,4 +19,17 @@ fun CalendarToolView.bindSelectedDay(day: String) {
 @BindingAdapter("bindCalendarSelectedListener")
 fun CalendarView.bindCalendarSelectedListener(listener: CalendarView.OnCalendarSelectListener) {
     setOnCalendarSelectListener(listener)
+}
+
+
+@BindingAdapter("bindTagImageSrc")
+fun ImageView.bindTagImageSrc(type: TaskType) {
+    val src = when (type) {
+        TaskType.WORK -> R.mipmap.ic_tag_work
+        TaskType.ENTERTAINMENT -> R.mipmap.ic_tag_game
+        TaskType.LIFE -> R.mipmap.ic_tag_life
+        TaskType.HEALTH -> R.mipmap.ic_tag_health
+        else -> R.mipmap.ic_tag_no_type
+    }
+    setImageResource(src)
 }
