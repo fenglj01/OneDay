7b73363bf0404b633e1cc6677d77aa2a72258333
fenglj <Lee_Mofeel@163.com>
2020-07-01 11:18:58 +0800

    -- SearchFragment layout

--- app/src/main/java/com/knight/oneday/search/SearchFragment.kt
+++ app/src/main/java/com/knight/oneday/search/SearchFragment.kt
@@ -1,12 +1,13 @@
 package com.knight.oneday.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.knight.oneday.R
+import com.knight.oneday.data.Task
+import com.knight.oneday.databinding.FragmentSearchBinding
+import com.knight.oneday.task.TaskAdapter
 import com.knight.oneday.utilities.InjectorUtils
 
 /**
@@ -21,12 +22,31 @@ class SearchFragment : Fragment() {
         )
     }
 
+    private lateinit var binding: FragmentSearchBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.searchTaskList.adapter = TaskAdapter(object :TaskAdapter.TaskEventListener{
+            override fun onTaskClicked(view: View, task: Task) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTaskLongClicked(task: Task): Boolean {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTaskStatusChanged(task: Task, isDone: Boolean) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
 }
