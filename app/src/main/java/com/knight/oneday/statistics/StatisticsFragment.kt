e429c523a776e6aa6c10d1f734c65c7544e5990b
fenglj <Lee_Mofeel@163.com>
2020-07-22 14:51:42 +0800

    -- v.1.0.1

---
+++ app/src/main/java/com/knight/oneday/statistics/StatisticsFragment.kt
@@ -0,0 +1,11 @@
+package com.knight.oneday.statistics
+
+import androidx.fragment.app.Fragment
+
+/**
+ * Create by FLJ in 2020/7/22 13:47
+ * 统计界面
+ */
+class StatisticsFragment : Fragment() {
+
+}
